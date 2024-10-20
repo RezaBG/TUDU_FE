81350885ebacf55a1c6faa9e9e5c1a626b014c06
Reza Barzegar <rezabarzegar@Rezas-MacBook-Pro.local>
2024-10-20 10:46:07 +0200

    Added todo fetching and display functionality with react-query

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,19 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
 import TodoItem from "./TodoItem.tsx";
 
-interface Todo {
-  id: number;
-  title: string;
-  description: string;
-}
+const fetchTodos = async () => {
+  const token = localStorage.getItem("access_token");
+  const response = await fetch("http://127.0.0.1:8000/todos", {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 
-interface TodoListProps {
-  todos: Todo[];
-}
+  if (!response.ok) {
+    throw new Error("Failed to fetch todos");
+  }
+  return response.json();
+};
 
-const TodoList = ({ todos }: TodoListProps) => {
+const TodoList = () => {
+  const {
+    data: todos,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchTodos,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error instanceof Error) return <div>{error.message}</div>;
+
   return (
     <div>
-      {todos.map((todo) => (
+      {todos.map((todo: { id: number; title: string; description: string }) => (
         <TodoItem
           key={todo.id}
           title={todo.title}
